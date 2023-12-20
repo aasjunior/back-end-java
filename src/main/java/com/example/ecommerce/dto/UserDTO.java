ceb074211d2792abf82843e806464ee1943708bf
aasjunior <adeldivo@hotmail.com>
2023-12-20 19:26:39 -0300

    4.2 Camada de Dados (Repository) - pg. 33 - 36

--- src/main/java/com/example/ecommerce/dto/UserDTO.java
+++ src/main/java/com/example/ecommerce/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.dto;
 
+import com.example.ecommerce.model.User;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,4 +23,15 @@ public class UserDTO {
     private String email;
     private String telefone;
     private LocalDateTime dataCadastro;
+
+    public static UserDTO convert(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setNome(user.getNome());
+        userDTO.setEndereco(user.getEndereco());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setTelefone(user.getTelefone());
+        userDTO.setDataCadastro(user.getDataCadastro());
+        return userDTO;
+    }
 }
