6aa2dbd1c5c6daabffe824b76471b0173da154be
aasjunior <adeldivo@hotmail.com>
2024-02-04 11:48:43 -0300

    5.2 CAMADA DE SERVIÇOS (SERVICE)

---
+++ product-api/src/main/java/com/aasjunior/ecommerce/productapi/service/ProductService.java
@@ -0,0 +1,74 @@
+package com.aasjunior.ecommerce.productapi.service;
+
+import com.aasjunior.ecommerce.productapi.dto.ProductDTO;
+import com.aasjunior.ecommerce.productapi.model.Product;
+import com.aasjunior.ecommerce.productapi.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public List<ProductDTO> getAll(){
+        List<Product> products = productRepository.findAll();
+        return products
+                .stream()
+                .map(ProductDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductDTO> getProductByCategoryId(Long categoryId){
+        List<Product> products = productRepository.getProductByCategory(categoryId);
+        return products
+                .stream()
+                .map(ProductDTO::convert)
+                .collect(Collectors.toList());
+    }
+
+    public ProductDTO findByProductIdentifier(String productIdentifier){
+        Product product = productRepository.findByProductIdentifier(productIdentifier);
+        if(product != null){
+            return ProductDTO.convert(product);
+        }
+        return null;
+    }
+
+    public ProductDTO save(ProductDTO productDTO){
+        Product product = productRepository.save(Product.convert(productDTO));
+        return ProductDTO.convert(product);
+    }
+
+    public void delete(long productId){
+        Optional<Product> product = productRepository.findById(productId);
+        if(product.isPresent()){
+            productRepository.delete(product.get());
+        }
+    }
+
+    public ProductDTO editProduct(long id, ProductDTO productDTO){
+        Product product = productRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+        if(productDTO.getName() != null || !productDTO.getName().isEmpty()){
+            product.setName(productDTO.getName());
+        }
+        if(productDTO.getPrice() != null){
+            product.setPrice(productDTO.getPrice());
+        }
+        return ProductDTO.convert(productRepository.save(product));
+    }
+
+    public Page<ProductDTO> getAllPage(Pageable page){
+        Page<Product> users = productRepository.findAll(page);
+        return users
+                .map(ProductDTO::convert);
+    }
+}
